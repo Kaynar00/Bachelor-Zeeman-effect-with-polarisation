fb4774670c20c1f00dce39a8df47a72df589a178
Kaynar00 <keyurvithlani10@gmail.com>
2022-04-08 15:08:49 +0200

    Exercise 4 and 5
    Did exercise 4 and 5

--- src/ExerciseW3.py
+++ src/ExerciseW3.py
@@ -46,3 +46,47 @@ for i in range(4):
 plt.title('Exercise 3')
 plt.savefig('ExerciseW3_3.pdf')
 plt.show()
+
+#I has its minima at another location and Q,U,V is 0
+
+#Exercise 4
+
+I40,Q40,U40,V40 = UR(a,b,B,x,l_0,ddopller,0,0.05,m.radians(45),m.radians(0),10,2,2,1,2,2,2)
+I445,Q445,U445,V445 = UR(a,b,B,x,l_0,ddopller,0,0.05,m.radians(45),m.radians(45),10,2,2,1,2,2,2)
+I490,Q490,U490,V490 = UR(a,b,B,x,l_0,ddopller,0,0.05,m.radians(45),m.radians(90),10,2,2,1,2,2,2)
+
+UR40lst = [I40,Q40,U40,V40]
+UR445lst = [I445,Q445,U445,V445]
+UR490lst = [I490,Q490,U490,V490]
+
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    #plt.plot(x,URlst[i],label='Exercise 2 '+Stokeslabel[i])
+    #plt.plot(x,UR3lst[i],label='Exercise 3 '+Stokeslabel[i])
+    plt.plot(x,UR40lst[i],label='Exercise 4, Xi=0 '+Stokeslabel[i])
+    plt.plot(x,UR445lst[i],label='Exercise 4, Xi=45 '+Stokeslabel[i])
+    plt.plot(x,UR490lst[i],label='Exercise 4, Xi=90 '+Stokeslabel[i])
+    plt.legend()
+plt.title('Exercise 4')
+plt.savefig('ExerciseW3_4.pdf')
+plt.show()
+
+#In I and V they don't depend on Xi but in Q and U they do
+
+I50,Q50,U50,V50 = UR(a,b,B,x,l_0,ddopller,0,0.05,m.radians(0),m.radians(30),10,2,2,1,2,2,2)
+I545,Q545,U545,V545 = UR(a,b,B,x,l_0,ddopller,0,0.05,m.radians(45),m.radians(30),10,2,2,1,2,2,2)
+I590,Q590,U590,V590 = UR(a,b,B,x,l_0,ddopller,0,0.05,m.radians(90),m.radians(30),10,2,2,1,2,2,2)
+
+UR50lst = [I50,Q50,U50,V50]
+UR545lst = [I545,Q545,U545,V545]
+UR590lst = [I590,Q590,U590,V590]
+
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    plt.plot(x,UR40lst[i],label='Exercise 5, Theta=0 '+Stokeslabel[i])
+    plt.plot(x,UR445lst[i],label='Exercise 5, Theta=45 '+Stokeslabel[i])
+    plt.plot(x,UR490lst[i],label='Exercise 5, Theta=90 '+Stokeslabel[i])
+    plt.legend()
+plt.title('Exercise 4')
+plt.savefig('ExerciseW3_5.pdf')
+plt.show()
