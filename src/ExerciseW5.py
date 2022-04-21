3e8775bf226b8950bede923e5aca5d30620646da
Kaynar00 <keyurvithlani10@gmail.com>
2022-04-21 16:00:19 +0200

    Update ExerciseW5.py
    plot

--- src/ExerciseW5.py
+++ src/ExerciseW5.py
@@ -1,7 +1,20 @@
+#%%
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 obstokes = np.load(r"C:\Users\keyur\OneDrive\Dokument\Zeemandata\observed_stokes.npy")
 obwaves = np.load(r"C:\Users\keyur\OneDrive\Dokument\Zeemandata\observed_wave.npy")
 cavity = np.load(r"C:\Users\keyur\OneDrive\Dokument\Zeemandata\cavity_map.npy")
 
-print(cavity)
+
+# %%
+#exercise
+xvalue = 300
+yvalue = 300
+
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    plt.plot(obstokes[i,:,yvalue,xvalue])
+plt.show()
+# %%
