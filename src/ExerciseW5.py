c5508a7d529a296d67263b4d7124aeec8364e54e
Kaynar00 <keyurvithlani10@gmail.com>
2022-04-21 16:00:04 +0200

    Update ExerciseW5.py
    Fixed so that the profiles from the data are plotted for one pixel

---
+++ src/ExerciseW5.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+obstokes = np.load(r"C:\Users\keyur\OneDrive\Dokument\Zeemandata\observed_stokes.npy")
+obwaves = np.load(r"C:\Users\keyur\OneDrive\Dokument\Zeemandata\observed_wave.npy")
+cavity = np.load(r"C:\Users\keyur\OneDrive\Dokument\Zeemandata\cavity_map.npy")
+
+print(cavity)
