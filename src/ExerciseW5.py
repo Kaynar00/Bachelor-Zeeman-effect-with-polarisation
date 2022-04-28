02b233caf234d10dcdec52e4fcd92b29a6527dc1
Kaynar00 <keyurvithlani10@gmail.com>
2022-04-28 15:03:44 +0200

    Update
    Changed the limit on the azimuth angle, fixed so that imshow doesn't interpolate and fixed the grafs a bit

--- src/ExerciseW5.py
+++ src/ExerciseW5.py
@@ -2,9 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from inversion import inversion
-from lmfit import Parameters, report_fit, fit_report
+from lmfit import Parameters, report_fit, fit_report, minimize
 import math as m
 from tqdm import tqdm
+from Tranpol import UR
 
 
 obstokes = np.load(r"C:\Users\keyur\OneDrive\Dokument\Zeemandata\observed_stokes.npy")
@@ -15,6 +16,35 @@ cavity = np.load(r"C:\Users\keyur\OneDrive\Dokument\Zeemandata\cavity_map.npy")
 # %%
 
 plt.imshow(obstokes[0,0,:,:])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('I')
+plt.colorbar()
+plt.savefig('Istokesdata.pdf')
+plt.show()
+
+plt.imshow(obstokes[1,0,:,:])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Q')
+plt.colorbar()
+plt.savefig('Qstokesdata.pdf')
+plt.show()
+
+plt.imshow(obstokes[2,0,:,:])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('U')
+plt.colorbar()
+plt.savefig('Ustokesdata.pdf')
+plt.show()
+
+plt.imshow(obstokes[3,0,:,:])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('V')
+plt.colorbar()
+plt.savefig('Vstokesdata.pdf')
 plt.show()
 
 #exercise
@@ -56,7 +86,7 @@ params.add('theta',value=m.radians(30),min=m.radians(0),max=m.radians(180))
 params.add('Xi',value=m.radians(30),min=m.radians(0),max=m.radians(360))
 params.add('eta_0',value=10,min=0,max=30)
 
-result, finala, params = inversion(params,l_0,J_l,J_u,L_l,L_u,S_l,S_u,obwaves[:15],datar,weight)
+result, finala = inversion(params,l_0,J_l,J_u,L_l,L_u,S_l,S_u,obwaves[:15],datar,weight)
 
 report_fit(result)
 
@@ -72,6 +102,7 @@ for i in range(4):
 plt.show()
 # %%
 plt.imshow(obstokes[0,0,:,:])
+plt.colorbar()
 plt.show()
 
 #exercise
@@ -113,12 +144,14 @@ params.add('theta',value=m.radians(30),min=m.radians(0),max=m.radians(180))
 params.add('Xi',value=m.radians(30),min=m.radians(0),max=m.radians(360))
 params.add('eta_0',value=10,min=0,max=30)
 
-result, finala, params = inversion(params,l_0,J_l,J_u,L_l,L_u,S_l,S_u,obwaves[:15],datar,weight)
+result, finala = inversion(params,l_0,J_l,J_u,L_l,L_u,S_l,S_u,obwaves[:15],datar,weight)
 
 report_fit(result)
 
 with open("quietsun.txt","w") as f:
     f.write(fit_report(result))
+
+final = finala.reshape(data.shape[0],data.shape[1])
 
 for i in range(4):
     plt.subplot(2,2,i+1)
@@ -169,12 +202,14 @@ params.add('theta',value=m.radians(30),min=m.radians(0),max=m.radians(180))
 params.add('Xi',value=m.radians(30),min=m.radians(0),max=m.radians(360))
 params.add('eta_0',value=10,min=0,max=30)
 
-result, finala, params_fit = inversion(params,l_0,J_l,J_u,L_l,L_u,S_l,S_u,obwaves[:15],datar,weight)
+result,finala = inversion(params,l_0,J_l,J_u,L_l,L_u,S_l,S_u,obwaves[:15],datar,weight)
 
 report_fit(result)
 
 with open("penumbra.txt","w") as f:
     f.write(fit_report(result))
+
+final = finala.reshape(data.shape[0],data.shape[1])
 
 for i in range(4):
     plt.subplot(2,2,i+1)
@@ -187,7 +222,7 @@ plt.show()
 skip_factor = 7
 sdata = obstokes[:,:15,0::skip_factor,0::skip_factor]
 
-result = np.empty((4,15,134,132,9))
+result = np.empty((134,132,9))
 
 for y in tqdm(range(134)):
     for x in range(132):
@@ -210,20 +245,156 @@ for y in tqdm(range(134)):
         S_u = 2
 
         params = Parameters()
-        params.add('a_',value=0.5,min=0,max=10)
+        params.add('a_',value=0.5,min=0,max=3)
         params.add('b_',value=0.5,min=0,max=10)
-        params.add('B_',value=100,min=0,max=20000)
-        params.add('ddopller_',value=0.05,min=0,max=1)
-        params.add('v_LOS',value=0,min=-30,max=30)
+        params.add('B_',value=100,min=0,max=3000)
+        params.add('ddopller_',value=0.05,min=0,max=0.2)
+        params.add('v_LOS',value=0,min=-5,max=5)
         params.add('aimag',value=0.2,min=0,max=1)
         params.add('theta',value=m.radians(30),min=m.radians(0),max=m.radians(180))
-        params.add('Xi',value=m.radians(30),min=m.radians(0),max=m.radians(360))
+        params.add('Xi',value=m.radians(30),min=m.radians(0),max=m.radians(180))
         params.add('eta_0',value=10,min=0,max=30)
 
-        result, finala, params = inversion(params,l_0,J_l,J_u,L_l,L_u,S_l,S_u,obwaves[:15],datar,weight)
+        def fcn2min(params, x_, data_,weight_):
+            """Model a decaying sine wave and subtract data."""
+            a__ = params['a_']
+            b__ = params['b_']
+            B__ = params['B_']
+            ddopller__ = params['ddopller_']
+            v_LOS__ = params['v_LOS']
+            aimag__ = params['aimag']
+            theta__ = params['theta']
+            Xi__ = params['Xi']
+            eta_0__ = params['eta_0']
+            model = UR(a__,b__,B__,x_,l_0,ddopller__,v_LOS__,aimag__,theta__,Xi__,eta_0__,J_l,J_u,L_l,L_u,S_l,S_u)
+            models = np.array(list(model[0])+list(model[1])+list(model[2])+list(model[3]))
+            return weight_*(models - data_)
 
+        #do fit here with leastsq algorithm
+        resultr = minimize(fcn2min,params,args=(obwaves[:15],datar,weight),max_nfev=200)
 
+        #calculate final result
+        params_fit = resultr.params
 
+        a = params_fit['a_']
+        b = params_fit['b_']
+        B = params_fit['B_']
+        ddoppler = params_fit['ddopller_']
+        v_LOS = params_fit['v_LOS']
+        aimag = params_fit['aimag']
+        theta = params_fit['theta']
+        Xi = params_fit['Xi']
+        eta_0 = params_fit['eta_0']
 
+        result[y,x,0] = a.value
+        result[y,x,1] = b.value
+        result[y,x,2] = B.value
+        result[y,x,3] = ddoppler.value
+        result[y,x,4] = v_LOS.value
+        result[y,x,5] = aimag.value
+        result[y,x,6] = theta.value
+        result[y,x,7] = Xi.value
+        result[y,x,8] = eta_0.value
 
+np.save('result.npy',result)
+#%%
+from scipy import ndimage
+
+result = np.load('result.npy')
+
+kernelsize = 3
+
+result[:,:,0] = ndimage.gaussian_filter(result[:,:,0], kernelsize)
+
+result[:,:,1] = ndimage.gaussian_filter(result[:,:,1], kernelsize)
+
+result[:,:,2] = ndimage.gaussian_filter(result[:,:,2], kernelsize)
+
+result[:,:,3] = ndimage.gaussian_filter(result[:,:,3], kernelsize)
+
+result[:,:,4] = ndimage.gaussian_filter(result[:,:,4], kernelsize)
+
+result[:,:,5] = ndimage.gaussian_filter(result[:,:,5], kernelsize)
+
+result[:,:,6] = ndimage.gaussian_filter(result[:,:,6], kernelsize)
+
+A = ndimage.gaussian_filter(np.sin(2*result[:,:,2]),kernelsize)
+
+B = ndimage.gaussian_filter(np.cos(2*result[:,:,2]),kernelsize)
+
+result[:,:,7] = (np.arctan2(A,B)% (2*np.pi) )/2.
+
+result[:,:,8] = ndimage.gaussian_filter(result[:,:,8], kernelsize)
+
+plt.imshow(result[:,:,0],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('a')
+plt.colorbar()
+plt.savefig('a.pdf')
+plt.show()
+
+plt.imshow(result[:,:,1],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('b')
+plt.colorbar()
+plt.savefig('b.pdf')
+plt.show()
+
+plt.imshow(result[:,:,2],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('B')
+plt.colorbar()
+plt.savefig('B.pdf')
+plt.show()
+
+plt.imshow(result[:,:,3],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('doppler width')
+plt.colorbar()
+plt.savefig('doppler_width.pdf')
+plt.show()
+
+plt.imshow(result[:,:,4],cmap='bwr',interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('v_LOS')
+plt.colorbar()
+plt.savefig('v_LOS.pdf')
+plt.show()
+
+plt.imshow(result[:,:,5],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('aimag')
+plt.colorbar()
+plt.savefig('aimag.pdf')
+plt.show()
+
+plt.imshow(result[:,:,6],cmap = 'RdYlGn',interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('theta')
+plt.colorbar()
+plt.savefig('theta.pdf')
+plt.show()
+
+plt.imshow(result[:,:,7],cmap='twilight',interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Xi')
+plt.colorbar()
+plt.savefig('Xi.pdf')
+plt.show()
+
+plt.imshow(result[:,:,8],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('eta_0')
+plt.colorbar()
+plt.savefig('eta_0.pdf')
+plt.show()
 # %%
