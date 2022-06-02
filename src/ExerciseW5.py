bd04b2432aeda11e31b88a887a0d37e8ecfd5a77
Kaynar00 <keyurvithlani10@gmail.com>
2022-06-02 16:12:06 +0200

    Update

--- src/ExerciseW5.py
+++ src/ExerciseW5.py
@@ -18,34 +18,36 @@ cavity = np.load(r"C:\Users\keyur\OneDrive\Dokument\Zeemandata\cavity_map.npy")
 plt.imshow(obstokes[0,0,:,:])
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('I')
+plt.title('I/lc')
 plt.colorbar()
 plt.savefig('Istokesdata.pdf')
 plt.show()
 
-plt.imshow(obstokes[1,0,:,:])
+plt.imshow(obstokes[1,8,:,:])
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Q')
+plt.title('Q/lc')
 plt.colorbar()
 plt.savefig('Qstokesdata.pdf')
 plt.show()
 
-plt.imshow(obstokes[2,0,:,:])
+plt.imshow(obstokes[2,8,:,:])
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('U')
+plt.title('U/lc')
 plt.colorbar()
 plt.savefig('Ustokesdata.pdf')
 plt.show()
 
-plt.imshow(obstokes[3,0,:,:])
+plt.imshow(obstokes[3,5,:,:])
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('V')
+plt.title('V/lc')
 plt.colorbar()
 plt.savefig('Vstokesdata.pdf')
 plt.show()
+
+#%%
 
 #exercise
 xvalue = 400
@@ -83,10 +85,10 @@ params.add('b_',value=0.5,min=0,max=10)
 params.add('B_',value=100,min=0,max=20000)
 params.add('ddopller_',value=0.05,min=0,max=1)
 params.add('v_LOS',value=0,min=-30,max=30)
-params.add('aimag',value=0.2,min=0,max=1)
+params.add('aimag',value=100,min=0,max=1)
 params.add('theta',value=m.radians(30),min=m.radians(0),max=m.radians(180))
 params.add('Xi',value=m.radians(30),min=m.radians(0),max=m.radians(360))
-params.add('eta_0',value=10,min=0,max=30)
+params.add('eta_0',value=10,min=0,max=200)
 
 result, finala = inversion(params,l_0,J_l,J_u,L_l,L_u,S_l,S_u,obwaves[:15],datar,weight)
 
@@ -97,11 +99,16 @@ with open("umbra.txt","w") as f:
 
 final = finala.reshape(data.shape[0],data.shape[1])
 
+Stokeslabel = ['I/lc','Q/lc','U/lc','V/lc']
+
 for i in range(4):
     plt.subplot(2,2,i+1)
-    plt.plot(obwaves[:15],data[i],'+')
-    plt.plot(obwaves[:15],final[i])
-plt.subplots_adjust(wspace = space + 0.1, hspace = space)
+    plt.plot(obwaves[:15]-6.3015e3,data[i],'+',label='measured data')
+    plt.plot(obwaves[:15]-6.3015e3,final[i],label='The fit')
+    plt.xlabel('$\lambda-\lambda_0 \; (Å)$')
+    plt.ylabel(Stokeslabel[i])
+    plt.legend(prop = {'size':4})
+plt.subplots_adjust(wspace = space + 0.2, hspace = space + 0.1)
 plt.savefig('umbra.pdf')
 plt.show()
 # %%
@@ -110,8 +117,8 @@ plt.colorbar()
 plt.show()
 
 #exercise
-xvalue = 100
-yvalue = 100
+xvalue = 50
+yvalue = 50
 
 space = 0.3
 
@@ -148,7 +155,7 @@ params.add('v_LOS',value=0,min=-30,max=30)
 params.add('aimag',value=0.2,min=0,max=1)
 params.add('theta',value=m.radians(30),min=m.radians(0),max=m.radians(180))
 params.add('Xi',value=m.radians(30),min=m.radians(0),max=m.radians(360))
-params.add('eta_0',value=10,min=0,max=30)
+params.add('eta_0',value=0,min=10,max=200)
 
 result, finala = inversion(params,l_0,J_l,J_u,L_l,L_u,S_l,S_u,obwaves[:15],datar,weight)
 
@@ -159,11 +166,16 @@ with open("quietsun.txt","w") as f:
 
 final = finala.reshape(data.shape[0],data.shape[1])
 
+Stokeslabel = ['I/lc','Q/lc','U/lc','V/lc']
+
 for i in range(4):
     plt.subplot(2,2,i+1)
-    plt.plot(obwaves[:15],data[i],'+')
-    plt.plot(obwaves[:15],final[i])
-plt.subplots_adjust(wspace = space + 0.1, hspace = space)
+    plt.plot(obwaves[:15]-6.3015e3,data[i],'+',label='measured data')
+    plt.plot(obwaves[:15]-6.3015e3,final[i],label='The fit')
+    plt.xlabel('$\lambda-\lambda_0 \; (Å)$')
+    plt.ylabel(Stokeslabel[i])
+    plt.legend(prop = {'size':4})
+plt.subplots_adjust(wspace = space + 0.2, hspace = space + 0.1)
 plt.savefig('quietsun.pdf')
 plt.show()
 # %%
@@ -188,8 +200,8 @@ datar = data.reshape(data.shape[0]*data.shape[1])
 
 weight = np.ones_like(data)
 weight[0,:] = 2. / np.max(np.abs(data[0,:]))
-weight[1,:] = 1. / np.max(np.abs(data[1,:]))
-weight[2,:] = 1. / np.max(np.abs(data[2,:]))
+weight[1,:] = 3. / np.max(np.abs(data[1,:]))
+weight[2,:] = 3. / np.max(np.abs(data[2,:]))
 weight[3,:] = 1. / np.max(np.abs(data[3,:]))
 weight = weight.reshape(weight.shape[0]*weight.shape[1])
 
@@ -210,7 +222,7 @@ params.add('v_LOS',value=0,min=-30,max=30)
 params.add('aimag',value=0.2,min=0,max=1)
 params.add('theta',value=m.radians(30),min=m.radians(0),max=m.radians(180))
 params.add('Xi',value=m.radians(30),min=m.radians(0),max=m.radians(360))
-params.add('eta_0',value=10,min=0,max=30)
+params.add('eta_0',value=0,min=10,max=200)
 
 result,finala = inversion(params,l_0,J_l,J_u,L_l,L_u,S_l,S_u,obwaves[:15],datar,weight)
 
@@ -221,11 +233,18 @@ with open("penumbra.txt","w") as f:
 
 final = finala.reshape(data.shape[0],data.shape[1])
 
+Stokeslabel = ['I/lc','Q/lc','U/lc','V/lc']
+
+plt.subplots(figsize=(10,10))
+
 for i in range(4):
     plt.subplot(2,2,i+1)
-    plt.plot(obwaves[:15],data[i],'+')
-    plt.plot(obwaves[:15],final[i])
-plt.subplots_adjust(wspace = space + 0.1, hspace = space)
+    plt.plot(obwaves[:15]-6.3015e3,data[i],'+',label='measured data')
+    plt.plot(obwaves[:15]-6.3015e3,final[i],label='The fit')
+    plt.xlabel('$\lambda-\lambda_0 \; (Å)$')
+    plt.ylabel(Stokeslabel[i])
+    plt.legend(prop = {'size':7})
+plt.subplots_adjust(wspace = space + 0.2, hspace = space + 0.1)
 plt.savefig('penumbra.pdf')
 plt.show()
 # %%
@@ -264,10 +283,10 @@ for y in tqdm(range(134)):
         params.add('B_',value=100,min=0,max=3000)
         params.add('ddopller_',value=0.05,min=0,max=0.2)
         params.add('v_LOS',value=0,min=-5,max=5)
-        params.add('aimag',value=0.2,min=0,max=1)
+        params.add('aimag',value=0.2,min=0,max=100)
         params.add('theta',value=m.radians(30),min=m.radians(0),max=m.radians(180))
         params.add('Xi',value=m.radians(30),min=m.radians(0),max=m.radians(180))
-        params.add('eta_0',value=10,min=0,max=30)
+        params.add('eta_0',value=10,min=0,max=50)
 
         def fcn2min(params, x_, data_,weight_):
             """Model a decaying sine wave and subtract data."""
@@ -336,6 +355,8 @@ result[:,:,7] = (np.arctan2(A,B)% (2*np.pi) )/2.
 
 result[:,:,8] = ndimage.gaussian_filter(result[:,:,8], kernelsize)
 
+np.save('resultsmooth.npy',result)
+
 for y in tqdm(range(134)):
     for x in range(132):
         data = sdata[:,:,y,x]
@@ -362,10 +383,10 @@ for y in tqdm(range(134)):
         params.add('B_',value=result[y,x,2],min=0,max=3000)
         params.add('ddopller_',value=result[y,x,3],min=0,max=0.2)
         params.add('v_LOS',value=result[y,x,4],min=-5,max=5)
-        params.add('aimag',value=result[y,x,5],min=0,max=1)
+        params.add('aimag',value=result[y,x,5],min=0,max=100)
         params.add('theta',value=result[y,x,6],min=m.radians(0),max=m.radians(180))
         params.add('Xi',value=result[y,x,7],min=m.radians(0),max=m.radians(180))
-        params.add('eta_0',value=result[y,x,8],min=0,max=30)
+        params.add('eta_0',value=result[y,x,8],min=0,max=50)
 
         def fcn2min(params, x_, data_,weight_):
             """Model a decaying sine wave and subtract data."""
@@ -408,11 +429,76 @@ for y in tqdm(range(134)):
         result[y,x,7] = Xi.value
         result[y,x,8] = eta_0.value
 
-np.save('result.npy',result)
+np.save('resultetachange.npy',result)
 #%%
 from scipy import ndimage
 
 result = np.load('result.npy')
+
+name = ['$S_1$','$S_0$','$B$','$\Delta\lambda_D$','$v_{LOS}$','$a_D$',r'$\theta$',r'$\chi$','$\eta_0$']
+units = ['$(1)$','$(1)$','Gauss','Ångström','km/s','$(1)$','Radians','Radians','$(1)$']
+
+plt.subplots(figsize=(10,10))
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    if i == 4:
+        plt.imshow(result[:,:,i],cmap='bwr',interpolation = None)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 3:
+        plt.imshow(result[:,:,i],interpolation = None,vmax=0.07)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 6:
+        plt.imshow(result[:,:,i],cmap = 'RdYlGn',interpolation = None)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 7:
+        plt.imshow(result[:,:,i],cmap='twilight',interpolation = 'nearest')
+        cbar = plt.colorbar(label=units[i])
+    else:
+        plt.imshow(result[:,:,i],interpolation = None)
+        cbar = plt.colorbar(label=units[i])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(name[i])
+plt.subplots_adjust(hspace = 0.3,wspace=0.6)
+plt.savefig('result.pdf')
+plt.show()
+
+#%%
+from scipy import ndimage
+
+result = np.load('resultetachange.npy')
+
+name = ['$S_1$','$S_0$','$B$','$\Delta\lambda_D$','$v_{LOS}$','$a_D$',r'$\theta$',r'$\chi$','$\eta_0$']
+units = ['$(1)$','$(1)$','Gauss','Ångström','km/s','$(1)$','Radians','Radians','$(1)$']
+
+plt.subplots(figsize=(10,10))
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    if i == 4:
+        plt.imshow(result[:,:,i],cmap='bwr',interpolation = None)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 3:
+        plt.imshow(result[:,:,i],interpolation = None,vmax=0.06)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 5:
+        plt.imshow(result[:,:,i],interpolation = None,vmax=2)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 6:
+        plt.imshow(result[:,:,i],cmap = 'RdYlGn',interpolation = None)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 7:
+        plt.imshow(result[:,:,i],cmap='twilight',interpolation = 'nearest')
+        cbar = plt.colorbar(label=units[i])
+    else:
+        plt.imshow(result[:,:,i],interpolation = None)
+        cbar = plt.colorbar(label=units[i])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(name[i])
+plt.subplots_adjust(hspace = 0.3,wspace=0.6)
+plt.savefig('result_eta_max50.pdf')
+plt.show()
+#%%
 
 plt.imshow(result[:,:,0],interpolation = None)
 plt.xlabel('x')
@@ -483,6 +569,187 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.title('eta_0')
 plt.colorbar()
-plt.savefig('eta_0.pdf')
+plt.savefig('eta_0_max50.pdf')
+plt.show()
+# %%
+resultnosmooth = np.load('resultnosmooth.npy')
+
+name = ['$S_1$','$S_0$','$B$','$\Delta\lambda_D$','$v_{LOS}$','$a_D$',r'$\theta$',r'$\chi$','$\eta_0$']
+units = ['$(1)$','$(1)$','Gauss','Ångström','km/s','$(1)$','Radians','Radians','$(1)$']
+
+plt.subplots(figsize=(10,10))
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    if i == 4:
+        plt.imshow(resultnosmooth[:,:,i],cmap='bwr',interpolation = None)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 3:
+        plt.imshow(result[:,:,i],interpolation = None,vmax=0.07)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 5:
+        plt.imshow(result[:,:,i],interpolation = None,vmax=2)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 6:
+        plt.imshow(resultnosmooth[:,:,i],cmap = 'RdYlGn',interpolation = None)
+        cbar = plt.colorbar(label=units[i])
+    elif i == 7:
+        plt.imshow(resultnosmooth[:,:,i],cmap='twilight',interpolation = 'nearest')
+        cbar = plt.colorbar(label=units[i])
+    else:
+        plt.imshow(result[:,:,i],interpolation = None)
+        cbar = plt.colorbar(label=units[i])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(name[i])
+plt.subplots_adjust(hspace = 0.3,wspace=0.6)
+plt.savefig('resultnosmooth.pdf')
+plt.show()
+
+#%%
+
+plt.imshow(resultnosmooth[:,:,0],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('a')
+plt.colorbar()
+plt.savefig('a_no_smooth.pdf')
+plt.show()
+
+plt.imshow(resultnosmooth[:,:,1],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('b')
+plt.colorbar()
+plt.savefig('b_no_smooth.pdf')
+plt.show()
+
+plt.imshow(resultnosmooth[:,:,2],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('B')
+plt.colorbar()
+plt.savefig('mag_no_smooth.pdf')
+plt.show()
+
+plt.imshow(resultnosmooth[:,:,3],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('doppler width')
+plt.colorbar()
+plt.savefig('doppler_width_no_smooth.pdf')
+plt.show()
+
+plt.imshow(resultnosmooth[:,:,4],cmap='bwr',interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('v_LOS')
+plt.colorbar()
+plt.savefig('v_LOS_no_smooth.pdf')
+plt.show()
+
+plt.imshow(resultnosmooth[:,:,5],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('aimag')
+plt.colorbar()
+plt.savefig('aimag_no_smooth.pdf')
+plt.show()
+
+plt.imshow(resultnosmooth[:,:,6],cmap = 'RdYlGn',interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('theta')
+plt.colorbar()
+plt.savefig('theta_no_smooth.pdf')
+plt.show()
+
+plt.imshow(resultnosmooth[:,:,7],cmap='twilight',interpolation = 'nearest')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Xi')
+plt.colorbar()
+plt.savefig('Xi_no_smooth.pdf')
+plt.show()
+
+plt.imshow(resultnosmooth[:,:,8],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('eta_0')
+plt.colorbar()
+plt.savefig('eta_0_no_smooth.pdf')
+plt.show()
+# %%
+resultsmooth = np.load('resultsmooth.npy')
+
+plt.imshow(resultsmooth[:,:,0],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('a')
+plt.colorbar()
+plt.savefig('a_smooth.pdf')
+plt.show()
+
+plt.imshow(resultsmooth[:,:,1],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('b')
+plt.colorbar()
+plt.savefig('b_smooth.pdf')
+plt.show()
+
+plt.imshow(resultsmooth[:,:,2],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('B')
+plt.colorbar()
+plt.savefig('mag_smooth.pdf')
+plt.show()
+
+plt.imshow(resultsmooth[:,:,3],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('doppler width')
+plt.colorbar()
+plt.savefig('doppler_width_smooth.pdf')
+plt.show()
+
+plt.imshow(resultsmooth[:,:,4],cmap='bwr',interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('v_LOS')
+plt.colorbar()
+plt.savefig('v_LOS_smooth.pdf')
+plt.show()
+
+plt.imshow(resultsmooth[:,:,5],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('aimag')
+plt.colorbar()
+plt.savefig('aimag_smooth.pdf')
+plt.show()
+
+plt.imshow(resultsmooth[:,:,6],cmap = 'RdYlGn',interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('theta')
+plt.colorbar()
+plt.savefig('theta_smooth.pdf')
+plt.show()
+
+plt.imshow(resultsmooth[:,:,7],cmap='twilight',interpolation = 'nearest')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Xi')
+plt.colorbar()
+plt.savefig('Xi_smooth.pdf')
+plt.show()
+
+plt.imshow(resultsmooth[:,:,8],interpolation = None)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('eta_0')
+plt.colorbar()
+plt.savefig('eta_0_smooth.pdf')
 plt.show()
 # %%
