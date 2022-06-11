56b33b554dbce1a13407bda8fc87a9950615e315
Kaynar00 <keyurvithlani10@gmail.com>
2022-06-11 08:32:23 +0200

    Update

--- src/ExerciseW3.py
+++ src/ExerciseW3.py
@@ -71,7 +71,7 @@ for i in range(4):
     plt.plot(x-6301.5,URlst[i],label=Stokeslabel[i]+', B=1000')
     plt.plot(x-6301.5,UR3000lst[i],label=Stokeslabel[i]+', B=10000G')
     plt.plot(x-6301.5,UR10000lst[i],label=Stokeslabel[i]+', B=10000G but 5D2-7D3')
-    plt.xlabel('$\lambda \; (Å)$')
+    plt.xlabel('$\lambda-\lambda_0 \; (Å)$')
     plt.ylabel(Stokeslabel[i])
     plt.legend(prop = {'size':4})
 plt.subplots_adjust(wspace = space + 0.1, hspace = space+0.2)
